2c5c765458cd74e405ff975c48e20b2ea94d304c
hongbowu <wuhongbo87@gmail.com>
2023-12-06 16:57:54 -0500

    add function to get fahrenheit and add some data

--- script.js
+++ script.js
@@ -5,15 +5,27 @@ let cityCollector = $("#cityCollect");
 let cityName = [];
 apiKey = "7b80215f2592be31ee5ecbe7b3f91df1";
 //functions
+function temperatureFahrenheit(data) {
+  return (((data - 273.15) * 9) / 5 + 32).toFixed(2);
+}
 function getApi() {
   input.val = "new york";
-  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?d=5q=${input.val}&appid=${apiKey}`;
+  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${input.val}&appid=${apiKey}`;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+
+      $(".card1 ul li#city").text(data.city.name);
+      $(".card1 ul li#temp").text(
+        `temp:${temperatureFahrenheit(data.list[0].main.temp)}\u2109`
+      );
+      $(".card1 ul li#wind").text(`wind:${data.list[0].wind.speed}MPH`);
+      $(".card1 ul li#humidity").text(
+        `humidity:${data.list[0].main.humidity}%`
+      );
     });
 }
 getApi();
