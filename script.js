c7ba7efb3f4e720a697779241f36dfc858e1e17b
hongbowu <wuhongbo87@gmail.com>
2023-12-05 22:57:59 -0500

    add Api url

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 let input = $("#searching");
 let button = $("button");
 let cityCollector = $("#cityCollect");
-
+let cityName = [];
 //functions
-
+function getApi() {
+  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=7b80215f2592be31ee5ecbe7b3f91df1`;
+  fetch(requestUrl)
+    .then(function (response) {
+      response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
 //storage
 
 //interaction
