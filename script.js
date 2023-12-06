51da4232676cd6695541b3405699eeed53a24d1c
hongbowu <wuhongbo87@gmail.com>
2023-12-05 23:35:58 -0500

    add some functions

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let cityCollector = $("#cityCollect");
 let cityName = [];
 //functions
 function getApi() {
-  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=7b80215f2592be31ee5ecbe7b3f91df1`;
+  cityName = "Newyork";
+  let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=7b80215f2592be31ee5ecbe7b3f91df1`;
   fetch(requestUrl)
     .then(function (response) {
       response.json();
@@ -14,6 +15,15 @@ function getApi() {
       console.log(data);
     });
 }
+getApi();
 //storage
-
+function getCity() {
+  localStorage.setItem("cityName", JSON.stringify(input.val));
+  cityName = cityName.push(input.val);
+}
+function logCity() {
+  let cityHolder = JSON.parse(localStorage.getItem("cityName"));
+  let newList = cityCollector.add("li");
+  newList.text = input.val;
+}
 //interaction
