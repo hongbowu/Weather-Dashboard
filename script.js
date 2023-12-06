cb7ecf49f37a9a73e954df9780cd9dd7c29b6254
hongbowu <wuhongbo87@gmail.com>
2023-12-06 16:06:44 -0500

    add functions and steps todo

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let input = $("#searching");
 let button = $("button");
 let cityCollector = $("#cityCollect");
 let cityName = [];
+apiKey = "7b80215f2592be31ee5ecbe7b3f91df1";
 //functions
 function getApi() {
-  cityName = "Newyork";
-  let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=7b80215f2592be31ee5ecbe7b3f91df1`;
+  input.val = "new york";
+  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?d=5q=${input.val}&appid=${apiKey}`;
   fetch(requestUrl)
     .then(function (response) {
-      response.json();
+      return response.json();
     })
     .then(function (data) {
       console.log(data);
@@ -18,12 +19,24 @@ function getApi() {
 getApi();
 //storage
 function getCity() {
+  // check to see if cityName already exists in local storage
+  // if it already exists, you need to parse the existing array
+  // then add new city
+  // then stringify
+  // then set localstorage to new array
+  // if it doesn't exist yet
+  // use new array and add new city to it
+  // then stringify
+  // then set localstorage
   localStorage.setItem("cityName", JSON.stringify(input.val));
   cityName = cityName.push(input.val);
 }
 function logCity() {
   let cityHolder = JSON.parse(localStorage.getItem("cityName"));
-  let newList = cityCollector.add("li");
-  newList.text = input.val;
+  // loop through cityholder
+  const listItem = $("<li>");
+  listItem.text();
+  cityCollector.append(listItem);
 }
+
 //interaction
